93454af878de6c8c635680648ba22c7f86e696dd
Gakarot <startradingcentre@gmail.com>
2025-05-22 13:27:33 +0530

    Update DiamondHands.s.sol

--- DiamondHands/DiamondHands.s.sol
+++ DiamondHands/DiamondHands.s.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+import "forge-std/Script.sol";
+import "../src/DiamondHands.sol";
+
+contract DeployScript is Script {
+    function run() external {
+
+        vm.startBroadcast();
+
+        // Deploy contract
+        DiamondHands dh = new DiamondHands();
+
+        vm.stopBroadcast();
+
+        console.log("Deployed at:", address(dh));
+    }
+}
 
