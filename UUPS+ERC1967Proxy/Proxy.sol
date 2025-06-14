c5a5db01d3a08a0c0c3d30da2713ad9cb67558ac
Gakarot <startradingcentre@gmail.com>
2025-06-14 18:29:18 +0530

    Update Proxy.sol

--- UUPS+ERC1967Proxy/Proxy.sol
+++ UUPS+ERC1967Proxy/Proxy.sol
@@ -7,11 +7,12 @@ contract MyProxy is ERC1967Proxy {
     constructor(
         address _logicAddress,
         string memory _name,
-        string memory _symbol
+        string memory _symbol,
+        address owner
     )
         ERC1967Proxy(
             _logicAddress,
-            abi.encodeWithSelector(bytes4(keccak256("initialize(string,string)")), _name, _symbol)
+            abi.encodeWithSelector(bytes4(keccak256("initialize(string,string)")), _name, _symbol,owner)
         )
     {}
 }
